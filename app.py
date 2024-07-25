4b98c57634b4a3c16f48756408c8888c764d1c56
Tharun Sai Reddy <75476727+tharunkunuthuru@users.noreply.github.com>
2024-07-25 10:57:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 import logging
 from flask import Flask, request, render_template, redirect, url_for, session
 from google.cloud import storage
+from google.oauth2 import service_account
 import os
 
 app = Flask(__name__)
 app.secret_key = 'a9b8c7d6e5f4a3b2c1d0e9f8d7c6b5a4'  # Replace with the generated secret key
 
 # Dummy credentials
-users = {'tharun': 'venky@12345', 'sunny': 'prem@125', 'yash': 'sunny@6163'}
+users = {'tharun': 'venky@12345', 'prem': 'sunny@6163', 'yash': 'prem@12345'}
 
 # Configure Google Cloud Storage
 bucket_name = "processed_data112"
+credentials_path = "C:/Users/tharu/Downloads/bold-camera-429007-i5-db9a9cd22bf5.json"
+
+credentials = service_account.Credentials.from_service_account_file(
+    credentials_path,
+    scopes=["https://www.googleapis.com/auth/cloud-platform"],
+)
+storage_client = storage.Client(credentials=credentials)
 
 def list_blobs_with_prefix(bucket_name, prefix):
-    storage_client = storage.Client()
     blobs = storage_client.list_blobs(bucket_name, prefix=prefix)
     return blobs
 
@@ -66,7 +73,6 @@ def download(blob_name):
     try:
         if 'username' not in session:
             return redirect(url_for('login'))
-        storage_client = storage.Client()
         bucket = storage_client.bucket(bucket_name)
         blob = bucket.blob(blob_name)
         url = blob.generate_signed_url(expiration=3600)
